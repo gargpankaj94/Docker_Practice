6c6db20ebaf6dee280f552e76ae5bb07c40fe728
gargpankaj94 <88105906+gargpankaj94@users.noreply.github.com>
2023-07-23 20:48:01 -0500

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,10 +1,19 @@
-!sudo apt update
-!apt-get install openjdk-8-jdk-headless -qq > /dev/null
-!wget -q https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz
+import os
+os.system('sudo apt update')
+os.system('apt-get install openjdk-8-jdk-headless -qq > /dev/null')
+os.system('wget -q https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz')
+os.system('tar xf spark-3.3.1-bin-hadoop3.tgz')
+os.system('pip install -q findspark')
+os.system('pip install pyspark')
 
-!tar xf spark-3.3.1-bin-hadoop3.tgz
-!pip install -q findspark
-!pip install pyspark
+#!sudo apt update
+#!apt-get install openjdk-8-jdk-headless -qq > /dev/null
+#!wget -q https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz
+
+#!tar xf spark-3.3.1-bin-hadoop3.tgz
+
+#!pip install -q findspark
+#!pip install pyspark
 
 import os
 os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
