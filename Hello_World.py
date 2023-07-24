7c0dc3103e07b5161ef518ae60353b074e73a3d8
gargpankaj94 <88105906+gargpankaj94@users.noreply.github.com>
2023-07-23 20:31:33 -0500

    Update Hello_World.py

--- Hello_World.py
+++ Hello_World.py
@@ -1,9 +1,20 @@
+!sudo apt update
+!apt-get install openjdk-8-jdk-headless -qq > /dev/null
+!wget -q https://dlcdn.apache.org/spark/spark-3.3.2/spark-3.3.2-bin-hadoop3.tgz
+
+!tar xf spark-3.3.1-bin-hadoop3.tgz
+!pip install -q findspark
+!pip install pyspark
+
+import os
+os.environ["JAVA_HOME"] = "/usr/lib/jvm/java-8-openjdk-amd64"
+os.environ["SPARK_HOME"] = "/content/spark-3.3.2-bin-hadoop3"
+
 from pyspark.sql import DataFrame, SparkSession
 from typing import List
 import pyspark.sql.types as T
 import pyspark.sql.functions as F
 from pyspark import SparkFiles
-
 
 spark = SparkSession \
        .builder \
